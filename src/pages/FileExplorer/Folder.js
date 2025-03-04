3cf73b7229fa8ae1024fdd849e581343fe84cb7a
Vibha27 <svibha081@gmail.com>
2025-03-04 23:05:25 +0530

    added add new file and folder icons with styling

--- src/pages/FileExplorer/Folder.js
+++ src/pages/FileExplorer/Folder.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FiFolderPlus } from "react-icons/fi";
+import { FiFilePlus } from "react-icons/fi";
 
 export const Folder = ({ folderData = {} }) => {
   const [expand, setExpand] = useState(false);
@@ -7,7 +9,10 @@ export const Folder = ({ folderData = {} }) => {
     return (
       <div>
         <div onClick={() => setExpand(!expand)} className="folder">
-          <span>📁 {folderData.name}</span>
+          <span>📁 {folderData.name}
+            <FiFilePlus size={18} className="folder-icons"/>
+            <FiFolderPlus   size={20} className="folder-icons"/>
+          </span>
         </div>
         {expand && (
           <div style={{ paddingLeft: "20px" }}>
